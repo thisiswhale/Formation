7616934f3ce90a14718a36da19a6037c840c8e9e
William Leung <leungwk25@gmail.com>
2023-11-08 22:37:15 -0800

    Update Get all subsequences.js

--- Backtracking/Get all subsequences.js
+++ Backtracking/Get all subsequences.js
@@ -12,92 +12,79 @@ getAllSubsequences("abc") ==
   ["a", "b", "c", "ab", "ac", "bc", "abc"]
  
 
-FUNCTION SIGNATURE
-function getAllSubsequences(word) {
-def getAllSubsequences(word: str) -> list[str]:
-'''
-
-
-"ab" -> ["a", "b", "ab"]
-
-base:
-builder, index -> 1, 2
-
-for a, b
-
-
-getAllSubs = (str) => {
-
-  result = []
-  curStr = []
-
-  helper = (index) => {
-    if(index == str.length) {
-      return
-    }
-
-    for loop over str
-      curStr.push(str[ind])
-      put curStr into result
-      helper(index+1)
-      curStr.pop()
-  }
-
-  helper(0);
-  return result
-}
+Time: O(2^n) to generate each of the 2 possibilities for all n positions
+Space: O(n) to create up to n stack frames. The output size is O(2n). 
+       Since the output is so large, it's worth separating out from the necessary space to do the rest of the work.
 
 */
 
+// Solution #1 Use array based backtracking
 const getAllSubs = (str) => {
 
   const result = []
   const currStr = []
 
   const helper = (index) => {
-    if(index == str.length) {
-      return
+    if(index === word.length) {
+       if(str.join('').length) {
+         result.push(str.join(''))
+       }
+       return
     }
-    
-    for(let i = index; i < str.length;i++){
 
-      currStr.push(str[i])
-      console.log(index, currStr)
-      result.push(currStr.join(""))
-      helper(i+1)
-      currStr.pop()
-    }
+    currStr.push(str[index])
+    helper(index+1)
+    currStr.pop()
+    helper(index+1)
   }
   helper(0);
   return result
 }
 
+// Solution #2 Use array based backtracking, with for loop
+const getAllSubs = (str) => {
+  const result = []
+  const currStr = []
+
+ const helper = (index) => {
+   if(index === str.length) return
+
+   for(let i = index; i < str.length; i++){
+     currStr.push(str[i])
+     result.push(currStr.join(""))
+     helper(i + 1)
+     currStr.pop()
+   }
+ }
+
+ helper(index)
+
+ return result
+}
+
+// Solution #3 Use string based backtracking
+const getAllSubs = (str) => {
+ const result = []
+ let currStr = ''
+
+ const helper = (index) => {
+   if(index === str.length) return
+
+   for(let i = index; i < str.length; i++){
+      currStr += str[i]
+      result.push(currStr)
+      helper(index + 1)
+      currStr = currStr.substring(0, currStr.length-1)
+   }
+ }
+
+ helper(index)
+ return result
+}
+
 console.log(getAllSubs('abc'))
 // console.log(getAllSubs(''))
 
-const getAllSubsWithPassingString = (str) => {
-
-  const result = []
-  
-  const helper = (index, currStr) => {
-    if(index == str.length) {
-      return
-    }
-    for(let i = index; i < str.length;i++){
-      currStr += str[i]
-      result.push(currStr)
-      helper(i+1, currStr)
-      currStr = currStr.substring(0, currStr.length-1);
-    }
-  }
-  helper(0, "");
-  return result
-}
-
-// console.log(getAllSubsWithPassingString('abc'))
-// console.log(getAllSubsWithPassingString(''))
-
-/*
 
 
 
