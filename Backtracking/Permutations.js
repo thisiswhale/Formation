8f969a52b62d4dfb138fdb2da6aaef50e5bb6250
William Leung <leungwk25@gmail.com>
2023-11-09 10:13:10 -0800

    Update Permutations.js

--- Backtracking/Permutations.js
+++ Backtracking/Permutations.js
@@ -25,102 +25,102 @@ def generatePermutations(nums: list[int]) -> list[list[int]]:
 '''
 */
 
-//[1,2,2,3]
-function generatePermutationsWithMap(nums) {
-  //  map<value, count> from nums
-  const map =  new Map()
-  for(let i of nums){
-    map.set( i, (map.get(i) ?? 0) + 1)
-  }
-  const result = []
-  const temp = []
-  function helper(){
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 
-    if(temp.length === nums.length){
-      return result.push([...temp])
+//APPROACH 1 : Swapping values in nums array
+var permute = function(nums) {
+    const permutations = []
+
+    function helper(start) {
+        if (start === nums.length - 1) {
+            permutations.push([...nums]);
+            return;
+        }
+
+        // Continue without making any changes to the order.
+        helper(start + 1);
+
+        // Now swap the current starting location with each remaining location.
+        // Must be sure to undo the swaps after each recursive call.
+        for (let i = start + 1; i < nums.length; i++) {
+            // swap(start, i);
+            [nums[start], nums[i]] = [nums[i], nums[start]];
+
+            helper(start + 1);
+            // swap(start, i);
+            [nums[start], nums[i]] = [nums[i], nums[start]];
+
+        }
+
     }
 
-    for(const [key, val] of map){
-      if(val > 0) {
-        temp.push(key)
-        map.set(key, val-1)
+    helper(0)
+
+    return permutations
+};
+
+//APPROACH 2 : Use Maps to check used values to push Path list to result
+
+var permute = function(nums) {
+
+  const ans = [];
+  const path = [];
+  function get_permute() {
+    if (path.length === nums.length) {
+        ans.push([...path]);
+        return;
+    }
+
+    for (let x of nums) {
+        if (!path.includes(x)) {
+            path.push(x);
+            get_permute();
+            path.pop();
+        }
+    }
+    
+  }
+
+  get_permute();
+  return ans;
+}
+
+//APPROACH 3 : Without Maps to check used values to push Path list to result
+
+var permute = function(nums) {
+
+  const result = []
+  const path = []
+  const used = new Set()
+
+  function helper(){
+
+    if(path.length === nums.length){
+        result.push([...path])
+        return 
+    }
+
+    for (let x of nums) {
+
+      if(!used.has(x)){
+        path.push(x)
+        used.add(x)
+
         helper()
-        temp.pop()
-        map.set(key, val)
+        
+        path.pop()
+        used.delete(x)
       }
+
     }
   }
 
   helper()
 
   return result
-  //  the output array
-  //  the "current" array, the state
-  // 
-  //  helper function
-  //    base case, if current array same length as nums, push copy of state into output array. return
-  // 
-  //    loop through keys in the map
-  //      check count to see if we still have instances to try
-  // 
-  //      "pick" decrement count in the map for the value, push value into state
-  //      recurse
-  //      "undo" increment count in the map for the value, pop value from state
-  // 
-  //  return output array
-}
-
-
-// function generatePermutations(nums) {
-//   //  map<value, count> from nums
-
-//   const result = []
-//   const temp = []
-//   const set = new Set()
-//   function helper(i){
-
-//     if(temp.length === nums.length){
-//       return result.push([...temp])
-//     }
-//     for(let i = 0; i < nums.length; i++){
-
-//       if(!set.has(i)){
-//         temp.push(nums[i])
-//         set.add(i)
-
-//         helper(i+1)
-        
-//         set.delete(i)
-//         temp.pop()
-//       }
-
-//     }
-//   }
-
-//   helper(0)
-
-//   return result
-// }
-
-function generatePermutations(nums) {
-  const ans = [];
-  function get_permute(part) {
-    if (part.length === nums.length) {
-      ans.push([...part]);
-      return;
-    } else {
-      for (let x of nums) {
-        if (!part.includes(x)) {
-          part.push(x);
-          get_permute(part);
-          part.pop();
-        }
-      }
-    }
-  }
-
-  get_permute([]);
-  return ans;
 }
 
 // console.log(generatePermutations([1,2,3,4]))
